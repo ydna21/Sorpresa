eb4163c00f528f02260de5e11039c1acce35d24c
ydna21 <andyodar212223@gmail.com>
2026-09-21 22:39:07 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('App lista para instalarse');
+});
+
+self.addEventListener('fetch', (e) => {
+  // El navegador pide esto para activar el botón de instalar
+});
